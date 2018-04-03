a23ab828f9598af579e85004db0c12dcf0de9b2a
Eli Ben-Michael <ebenmichael@berkeley.edu>
2018-04-03 15:22:08 -0700

    Implemented double selection for covariates

--- R/highdim.R
+++ R/highdim.R
@@ -281,3 +281,229 @@ get_progsyn <- function(outcomes, metadata, trt_unit=1,
     return(ctrls)
 }
     
+####### Apply a covariate screen then fit synth
+
+lasso_screen <- function(ipw_format, syn_format, opts=list(avg=FALSE)) {
+    #' Screen covariates for the outcome process
+    #'
+    #' @param ipw_format Output of `format_ipw`
+    #' @param syn_format Output of `syn_format`    r
+    #' @param opts List of options for glmnet:
+    #'             \itemize{\item{avg }{Fit the average post-period rather than time periods separately}}
+    #'
+    #'
+    #'
+    #' @return \itemize{
+    #'           \item{selX }{Selected covariates}
+    #'           \item{params }{Regression parameters}}
+
+    X <- ipw_format$X
+    y <- ipw_format$y
+    trt <- ipw_format$trt
+    
+    ## helper function to fit regression with CV
+    outfit <- function(x, y) {
+            lam <- glmnet::cv.glmnet(x, y, alpha=.25, intercept=FALSE)$lambda.min
+            fit <- glmnet::glmnet(x, y, alpha=.25,
+                                  lambda=lam, intercept=FALSE)
+            return(as.matrix(coef(fit))[-1,])
+    }
+
+    if(opts$avg) {
+        ## if fitting the average post period value, stack post periods together
+        stacky <- c(y)
+        stackx <- do.call(rbind,
+                          lapply(1:dim(y)[2],
+                                 function(x) X))
+        stacktrt <- rep(trt, dim(y)[2])
+        regweights <- outfit(stackx[stacktrt==0,],
+                             stacky[stacktrt==0])
+    } else {
+        ## fit separate regressions for each post period
+        regweights <- apply(y, 2,
+                            function(yt) outfit(X[trt==0,],
+                                                yt[trt==0]))
+    }
+
+    ## get covariates with non-zero regression weight
+    selected <- apply(regweights, 1, function(beta) 1 * (sum(abs(beta)) > 0))
+    ## only return those covariates
+    selX <- X[, selected == 1]
+    
+    return(list(selX=selX,
+                params=list(regparams=regweights,
+                            selected=selected)))
+}
+
+
+
+double_screen <- function(ipw_format, syn_format, opts=list(avg=FALSE, by=1)) {
+    #' Screen covariates for the outcome process with LASSO and selection with
+    #' SC with infinity norm
+    #'
+    #' @param ipw_format Output of `format_ipw`
+    #' @param syn_format Output of `syn_format`    r
+    #' @param opts List of options for glmnet:
+    #'             \itemize{\item{avg }{Fit the average post-period rather than time periods separately}
+    #'                      \item{by }{Step size for binary search of minimal Linfinity error}
+    #'
+    #'
+    #'
+    #' @return \itemize{
+    #'           \item{selX }{Selected covariates}
+    #'           \item{params }{Regression parameters}}
+
+    ## screen covariates for outcome process
+    lasout <- lasso_screen(ipw_format, syn_format, opts)
+
+    ## screen using L infinity
+
+    
+    ## create the feasibility function by changing the LASSO hyper parameter
+    feasfunc <- function(ep) {
+        suppressMessages(feas <- fit_entropy_formatted(syn_format, ep, lasso=TRUE)$feasible)
+        return(feas)
+    }
+
+    
+    ## find the best epsilon
+    minep <- bin_search(0, max(ipw_format$X), opts$by, feasfunc)
+
+    ## if it failed, then stop everything
+    if(minep < 0) {
+        stop("Failed to find a synthetic control with balance better than 4 std deviations")
+    }
+
+    ## fit with minep
+    suppressMessages(linfent <- fit_entropy_formatted(syn_format, minep, lasso=TRUE))
+    
+    ## get covariates with non-zero dual value
+    selected_p <- 1 * (abs(linfent$dual) > 0)
+
+    ## take union of outcome and selection covariates
+    selected <- 1 * ((selected_p + lasout$params$selected) > 0)
+
+    ## only return those covariates
+    selX <- ipw_format$X[, selected == 1]
+    
+    return(list(selX=selX,
+                params=list(reparams=lasout$params,
+                            selparams=linfent$dual,
+                            minep=minep,
+                            selected=selected)))
+}
+
+
+fit_screensyn_formatted <- function(ipw_format, syn_format,
+                                    screen_x, fit_weights,
+                                    opts.prog=NULL, opts.weights=NULL) {
+    #' Select covariates for E[Y(0)|X], then balance those
+    #'
+    #' @param ipw_format Output of `format_ipw`
+    #' @param syn_format Output of `syn_format`
+    #' @param fit_progscore Function to fit prognostic score
+    #' @param fit_weights Function to fit synth weights
+    #' @param opts.screen Optional options for covariate screening
+    #' @param opts.weights Optional options for fitting synth weights
+    #' 
+    #' @return inverse of predicted propensity scores
+    #'         outcome regression parameters
+    #'         control outcomes
+    #'         treated outcomes
+    #'         boolean for treated
+
+    ## fit prognostic scores
+    if(is.null(opts.prog)) {
+        fitout <- screen_x(ipw_format, syn_format)
+    } else {
+        fitout <- screen_x(ipw_format, syn_format, opts.prog)
+    }
+
+    selX <- fitout$selX
+    
+    ## replace outcomes with fitted prognostic scores
+    syn_format$synth_data$Z0 <- t(as.matrix(selX[ipw_format$trt == 0,]))
+    syn_format$synth_data$Z1 <- as.matrix(selX[ipw_format$trt == 1,])
+
+    ## fit synth/maxent weights
+    syn <- fit_weights(syn_format)
+
+    syn$params <- fitout$params
+    return(syn)
+}
+
+
+get_screensyn <- function(outcomes, metadata, trt_unit=1,
+                        screenfunc=c("LAS", "2"),
+                        weightfunc=c("SC","ENT"),
+                        opts.screen = NULL,
+                        opts.weights = NULL,
+                        outcome_col=NULL,
+                        cols=list(unit="unit", time="time",
+                                  outcome="outcome", treated="treated")) {
+    #' Fit synthetic controls on estimated outcomes under control
+    #' @param outcomes Tidy dataframe with the outcomes and meta data
+    #' @param metadata Dataframe of metadata
+    #' @param trt_unit Unit that is treated (target for regression), default: 0
+    #' @param screenfunc What function to use to impute control outcomes
+    #'                 LAS=LASSO on outcome model,
+    #'                 2=LASSO on outcome and L infinity dual on selection
+    #' @param weightfunc What function to use to fit weights
+    #'                   SC=Vanilla Synthetic Controls, ENT=Maximum Entropy
+    #' @param opts.screen Optional options for fitting prognostic score
+    #' @param opts.weights Optional options for fitting synth weights    
+    #' @param outcome_col Column name which identifies outcomes, if NULL then
+    #'                    assume only one outcome
+    #' @param cols Column names corresponding to the units,
+    #'             time variable, outcome, and treated indicator
+    #'
+    #' @return outcomes with additional synthetic control added and weights
+    #' @export
+
+    ## prognostic score and weight functions to use
+    if(screenfunc == "LAS") {
+        screenf <- lasso_screen
+    } else if(screenfunc == "2") {
+        screenf <- double_screen
+    } else {
+        stop("screen must be one of 'LAS', '2'")
+    }
+
+    if(weightfunc == "SC") {
+        weightf <- fit_synth_formatted
+    } else if(weightfunc == "ENT") {
+        weightf <- fit_entropy_formatted
+    }
+    
+    ## format data
+    ipw_format <- format_ipw(outcomes, metadata, outcome_col, cols)
+    syn_format <- format_data(outcomes, metadata, trt_unit, outcome_col, cols)
+
+    ## fit weights
+    out <- fit_screensyn_formatted(ipw_format, syn_format,
+                                 screenf, weightf,
+                                 opts.screen, opt.weights)
+                                 
+
+    ## match outcome types to synthetic controls
+    if(!is.null(outcome_col)) {
+        data_out$outcomes[[outcome_col]] <- factor(outcomes[[outcome_col]],
+                                          levels = names(out$groups))
+        data_out$outcomes <- data_out$outcomes %>% dplyr::arrange_(outcome_col)
+    }
+
+
+    ctrls <- impute_controls(syn_format$outcomes, out, trt_unit)
+
+    ctrls$params <- out$params
+    ctrls$dual <- out$dual
+    ctrls$primal_obj <- out$primal_obj
+    ctrls$pscores <- out$pscores
+    ctrls$eta <- out$eta
+    ctrls$groups <- out$groups
+    ctrls$feasible <- out$feasible
+    ctrls$primal_group_obj <- out$primal_group_obj
+    ctrls$scaled_primal_obj <- out$scaled_primal_obj
+    ctrls$controls <- out$controls
+    return(ctrls)
+}
