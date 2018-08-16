3d2849bceedcadb6ad871f173830f2d9f24ba54c
Eli Ben-Michael <ebenmichael@berkeley.edu>
2018-08-16 16:46:40 -0700

    Added Chernozhukov conformal inference

--- R/inference.R
+++ R/inference.R
@@ -530,6 +530,75 @@ last <- function(pre, post) {
 
 
 
+
+### Chernozhukov method
+#' Weighted Least Squares Standard Errors
+#' @param outcomes Tidy dataframe with the outcomes and meta data
+#' @param metadata Dataframe of metadata
+#' @param ns Number of samples from the permutation distribution to draw
+#' @param trt_unit Treated unit
+#' @param cols Column names corresponding to the units,
+#'             time variable, outcome, and treated indicator
+#' @param order of test statistic
+#'
+#' @return outcomes with additional synthetic control added and weights
+#' @export
+cherno_test <- function(outcomes, metadata, ns=1000,  q=c(2,1), trt_unit=1, 
+                        cols=list(unit="unit", time="time",
+                                  outcome="outcome", treated="treated")) {
+
+
+    ## format data for synth
+    syn_data <- format_data(outcomes, metadata, trt_unit, cols=cols)
+
+    ## pre and post outcomes
+
+    
+    trtmat <- syn_data$synth_data$Y0plot
+    ctrlmat <- syn_data$synth_data$Y1plot
+
+    t0 <- nrow(syn_data$synth_data$Z0)
+    t_final <- nrow(syn_data$synth_data$Y0plot)
+
+    teststats <- matrix(0, nrow=ns, ncol=length(q))
+
+    for(i in 1:ns) {
+
+        ## sample from permutation distribution
+        reorder <- sample(1:t_final, t_final)
+
+        ## fit synth with reordered time periods
+
+        new_synth_data <- syn_data
+
+        new_synth_data$synth_data$X0 <- syn_data$synth_data$Y0plot[reorder,,drop=FALSE][1:t0,]
+        new_synth_data$synth_data$X1 <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][1:t0,]
+
+        syn <- fit_synth_formatted(new_synth_data)
+
+        ## get treatment effect estimates
+
+        att <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] -
+            syn_data$synth_data$Y0plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] %*% syn$weights
+
+        teststats[i,] <- sapply(1:length(q),
+                                function(j) mean(abs(att)^q[j])^(1/q[j]))
+    }
+
+    ## compute test stat for actual data
+    syn <- fit_synth_formatted(syn_data)
+    real_att <-  syn_data$synth_data$Y1plot[(t0+1):t_final,,drop=FALSE] -
+        syn_data$synth_data$Y0plot[(t0+1):t_final,,drop=FALSE] %*% syn$weights
+    real_teststat <- sapply(1:length(q),
+                            function(j) mean(abs(real_att)^q[j])^(1/q[j]))
+    pvals <- sapply(1:length(q), function(i) mean(teststats[,i] >= real_teststat[i]))
+    
+    return(pvals)
+}
+
+
+
+
 ##### SAMPLING INFERENCE
 
 standard_error_ <- function(metadata, fitfunc, units, trt_unit,
