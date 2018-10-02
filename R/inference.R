976eb2bc01c812f9e9e003a6e07503eb41f4be8d
Eli Ben-Michael <ebenmichael@berkeley.edu>
2018-10-02 12:07:41 -0700

    Added block permutation to cherno method

--- R/inference.R
+++ R/inference.R
@@ -746,7 +746,8 @@ last <- function(pre, post) {
 #'
 #' @return outcomes with additional synthetic control added and weights
 #' @export
-cherno_test <- function(outcomes, metadata, ns=1000,  q=c(2,1), trt_unit=1, 
+cherno_test <- function(outcomes, metadata, ns=1000,  q=c(2,1), trt_unit=1,
+                        permtype=c("iid", "block"),
                         cols=list(unit="unit", time="time",
                                   outcome="outcome", treated="treated")) {
 
@@ -763,31 +764,56 @@ cherno_test <- function(outcomes, metadata, ns=1000,  q=c(2,1), trt_unit=1,
     t0 <- nrow(syn_data$synth_data$Z0)
     t_final <- nrow(syn_data$synth_data$Y0plot)
 
-    teststats <- matrix(0, nrow=ns, ncol=length(q))
+    if(permtype == "iid") {
+        teststats <- matrix(0, nrow=ns, ncol=length(q))
 
-    for(i in 1:ns) {
+        for(i in 1:ns) {
 
-        ## sample from permutation distribution
-        reorder <- sample(1:t_final, t_final)
+            ## sample from permutation distribution
+            reorder <- sample(1:t_final, t_final)
 
-        ## fit synth with reordered time periods
+            ## fit synth with reordered time periods
 
-        new_synth_data <- syn_data
+            new_synth_data <- syn_data
 
-        new_synth_data$synth_data$X0 <- syn_data$synth_data$Y0plot[reorder,,drop=FALSE][1:t0,]
-        new_synth_data$synth_data$X1 <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][1:t0,]
+            new_synth_data$synth_data$X0 <- syn_data$synth_data$Y0plot[reorder,,drop=FALSE][1:t0,]
+            new_synth_data$synth_data$X1 <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][1:t0,]
 
-        syn <- fit_synth_formatted(new_synth_data)
+            syn <- fit_synth_formatted(new_synth_data)
 
-        ## get treatment effect estimates
+            ## get treatment effect estimates
 
-        att <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] -
-            syn_data$synth_data$Y0plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] %*% syn$weights
+            att <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] -
+                syn_data$synth_data$Y0plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] %*% syn$weights
 
-        teststats[i,] <- sapply(1:length(q),
-                                function(j) mean(abs(att)^q[j])^(1/q[j]))
+            teststats[i,] <- sapply(1:length(q),
+                                    function(j) mean(abs(att)^q[j])^(1/q[j]))
+        }
+    } else if(permtype=="block") {
+        teststats <- matrix(0, nrow=t_final, ncol=length(q))
+
+        for(i in 1:t_final) {
+            ## increment time by one step and wrap
+            reorder <- (1:t_final + i) %% t_final + 1
+            ## fit synth with reordered time periods
+            new_synth_data <- syn_data
+
+            new_synth_data$synth_data$X0 <- syn_data$synth_data$Y0plot[reorder,,drop=FALSE][1:t0,]
+            new_synth_data$synth_data$X1 <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][1:t0,]
+
+            syn <- fit_synth_formatted(new_synth_data)
+
+            ## get treatment effect estimates
+
+            att <- syn_data$synth_data$Y1plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] -
+                syn_data$synth_data$Y0plot[reorder,,drop=FALSE][(t0+1):t_final,,drop=FALSE] %*% syn$weights
+
+            teststats[i,] <- sapply(1:length(q),
+                                    function(j) mean(abs(att)^q[j])^(1/q[j]))            
+        }
+    } else {
+        stop("permtype must be one of c('iid', 'block')")
     }
-
     ## compute test stat for actual data
     syn <- fit_synth_formatted(syn_data)
     real_att <-  syn_data$synth_data$Y1plot[(t0+1):t_final,,drop=FALSE] -
