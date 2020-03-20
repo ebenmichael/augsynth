d3cdae0e6dba6b6468050ac753e41ec89b687cb9
Alan Liang <alanliang@Alans-MBP.PK5001Z>
2020-03-20 16:28:51 -0700

    remove all errors

--- R/globalVariables.R
+++ R/globalVariables.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("time", "val", "post", "weight", ".", "Time", "Estimate", "Std.Error", "Level", "last_time", "is_avg", "label", "Outcome"))
+utils::globalVariables(c("time", "val", "post", "weight", ".", "Time", "Estimate", "Std.Error", "Level", "last_time", "is_avg", "label", "Outcome", "unit", "obs"))
